cdbb169154721688a1c72182a6000b7d4d37e68e
admin_tunnus <admin_tunnus>
2017-06-27 17:43:21 +0300

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
@@ -16,27 +16,14 @@ io.on('connection', (socket)=>{
 
 console.log('new user connected');
 
-socket.emit('newMessage', {
-	from: 'admin', 
-	text: 'welcome to the chat app',
-	createdAt: new Date().getTime()
-});
+socket.emit('newMessage', generateMessage('admin', 'welcome to the chat app'));
 
-socket.broadcast.emit('newMessage', {
-	from: 'admin', 
-	text: 'new user joined', 
-	createdAt: new Date().getTime()
-});
+socket.broadcast.emit('newMessage', generateMessage('admin', 'new user joined'));
 
 
 socket.on('createMessage', function(message) {
 console.log('createMessage', message);
-io.emit('newMessage', {
-from: message.from, 
-text: message.text, 
-createdAt: new Date().getTime()
-
-});
+io.emit('newMessage', generateMessage(message.from, message.text));
 /*socket.broadcast.emit('newMessage', {
 
 	from: message.from, 
